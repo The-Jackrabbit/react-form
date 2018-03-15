831934a80fe1a2b42529c4e8f08f01cd0a4e372d
The-Jackrabbit <lsm5fm@virginia.edu>
2018-03-15 14:19:30 -0400

    Moved input calculation to form.js

--- src/Input/input.js
+++ src/Input/input.js
@@ -2,57 +2,40 @@ import React, {Component} from "react";
 import PropTypes from "prop-types";
 import "./input.css";
 import PasswordTip from "./PasswordTip/passwordTip";
-import StatusIcon from "../../Icons/statusIcon";
+import StatusIcon from "../Icons/statusIcon";
 import ErrorMessage from "./ErrorMessage/errorMessage";
-/*
 
-Password needs 
-1) to update itself
-2) send out update to form 
-
-Form needs:
-1) take in input data
-2) update passwordConfirm 
-
-*/
 class Input extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			"value": null
-		};
-		this.handleChange = this.handleChange.bind(this);
-		this.handleFocus = this.handleFocus.bind(this);
-		this.handleBlur = this.handleBlur.bind(this);
-		this.checkPassword = this.checkPassword.bind(this);
-	}
-
-	componentWillMount() {
-		let newStateVars = {
+			"value": null,
 			"placeholder": this.props.name,
 			"value": this.props.value,
 			"isEmpty": this.props.value.length === 0,
 			"touched": false,
 			"labelIsVisible": false
 		};
-		if (this.props.context === "password") {
-			newStateVars["passwordProgress"] =  {};
-		}
-		if (this.props.isValid) {
-			newStateVars["isValid"] = this.props.isValid;
-		}
+
+		this.handleChange = this.handleChange.bind(this);
+		this.handleFocus = this.handleFocus.bind(this);
+		this.handleBlur = this.handleBlur.bind(this);
+	}
+
+	componentWillMount() {
+		let newStateVars = {};
+		newStateVars["passwordProgress"] = (this.props.context === "password") ? {} : null;
+		newStateVars["isValid"] = (this.props.isValid) ? this.props.isValid : null;
+
 		this.setState({
 			...newStateVars
 		});
 	}
 
 	componentWillReceiveProps(nextProps){
-		console.log("updating props for" + this.props.context);
-		console.log(`"showErrorMessage" for ${this.props.context}: ${this.state.touched && !nextProps.isValid}`);
+		// updates confirmPassword when password field is updated to match passwordConfirm field
 		if ("isValid" in nextProps) {
-			console.log(`"this.state.touched " for ${this.props.context}: ${this.state.touched}`);
-			console.log(`"!nextProps.isValid" for ${this.props.context}: ${!nextProps.isValid}`);
 			this.setState({
 				"isValid": nextProps.isValid,
 				"showErrorMessage": (this.state.touched || this.state.engaged) && !nextProps.isValid,
@@ -60,58 +43,19 @@ class Input extends Component {
 		}
 	}
 
-	checkPassword(value) {
-		let countCapitals = value.replace(/[^A-Z]/g, "").length;
-		let countNumbers = value.replace(/[^0-9]/g, "").length;
-		let countSymbols = value.replace(/[^&%$#@!?]/g, "").length;
-		let isValid = true;
-		let tests = {};
-
-		if (this.props.reqs.numberOfCapitals) {
-			tests["numberOfCapitals"] = this.props.reqs.numberOfCapitals <= countCapitals;
-		}
-		if (this.props.reqs.numberOfNumbers) {
-			tests["numberOfNumbers"] = this.props.reqs.numberOfNumbers <= countNumbers; 
-		}
-		if (this.props.reqs.numberOfSymbols) {
-			tests["numberOfSymbols"] = this.props.reqs.numberOfSymbols <= countSymbols; 
-		}
-		if (this.props.reqs.minimumLength) {
-			tests["minimumLength"] = this.props.reqs.minimumLength <= value.length; 
-		}
-		if (this.props.reqs.noRestrictedWords) {
-			tests["noRestrictedWords"] = !this.props.restrictedWords.includes(value); 
-		}
-
-		this.setState({
-			"passwordProgress": tests
-		});
-
-		const andAllReducer = (previous, currentTest) => previous&currentTest;
-		isValid = Object.values(tests).reduce(andAllReducer, isValid);
-
-		return isValid;
-	}
-
 	handleChange(event) {
-		console.log("handling change for" + this.props.context);
-		let newValidity = event.target.value.length > 0;
-		if (this.props.context === "password") {
-			newValidity = newValidity&&this.checkPassword(event.target.value);
-		}
-		else if (this.props.validator) {
-			newValidity = newValidity&&this.props.validator(event.target.value);
-		}
+		// call onChange method on the parent component for updating its state
+		this.props.onChange(event, this.props.context);
+		this.props.validator(event);
 
 		let updatedValues = {
 			"value": event.target.value,
 			"isEmpty": event.target.value.length < 1,
 			"touched": true,
-			"isValid": newValidity,
-			"showErrorMessage": !newValidity,
+			"showErrorMessage": !this.props.isValid,
 			"errorMessage": this.props.invalidText
 		};
-		console.log(`"showErrorMessage" for ${this.props.context}: ${!newValidity}`);
+
 		if (event.target.value.length < 1) {
 			updatedValues["errorMessage"] = this.props.emptyText;
 		}
@@ -119,11 +63,6 @@ class Input extends Component {
 		this.setState({
 			...updatedValues
 		});
-
-		// call onChange method on the parent component for updating it's state
-		if(this.props.onChange) {
-			this.props.onChange(event, newValidity, this.props.context);
-		}
 	}
 	
 	handleFocus() {
@@ -139,74 +78,61 @@ class Input extends Component {
 			"focus": false,
 			"placeholder": this.props.name,
 			"labelIsVisible": !this.state.isEmpty,
-			
 		});
-	}
-
-	shouldComponentUpdate(nextProps, nextState) {
-		if (this.props.context === "email") {
-			return this.nextState.value !== this.state.value;
-		}
-		return false;
 	}
 	
 	render() {
 		return (
-			<div>
-				<div className="input grid">
-					<div className="input-container grid">
-						<div className="input-area">
-							{this.state.labelIsVisible && 
-								<label className="input-label" htmlFor={this.props.name}>
-									<span className="label-text">{this.props.name}</span>
-								</label>
-							}
-							<input 
-								type={this.props.inputType}
-								id={this.props.name}
-								value={this.state.value} 
-								placeholder={this.state.placeholder}
-								onChange={this.handleChange}
-								onFocus={this.handleFocus}
-								onBlur={this.handleBlur}
-							/>
-							
-						</div>
-						<div className="status-icon-area">
-							{this.state.touched &&
-							<StatusIcon 
-								isValid={this.state.isValid}
-								height="18pt"
-								width="18pt"
-							></StatusIcon>}
-						</div>
-						
-					</div>
-					<div className="errorMessage">
-						
-						{ (this.state.focus && this.props.context === "password" && 
-							<div className="password-tip-container">
-								<PasswordTip
-									reqs={this.props.reqs}
-									passwordProgress={this.state.passwordProgress}
-									isValid={this.state.isValid}
-									restrictedWords={this.props.restrictedWords}
-									noRestrictedWords={this.state.noRestrictedWords}
-								></PasswordTip>
-							</div>
-						)
-							||
-							<ErrorMessage
-								isVisible={this.state.showErrorMessage}
-								errorMessage={this.state.errorMessage}>
-							</ErrorMessage>
+			<div className="input grid">
+				<div className="input-container grid">
+					<div className="input-area">
+						{this.state.labelIsVisible && 
+							<label className="input-label" htmlFor={this.props.name}>
+								<span className="label-text">{this.props.name}</span>
+							</label>
 						}
+						<input 
+							type={this.props.inputType}
+							id={this.props.name}
+							value={this.state.value} 
+							placeholder={this.state.placeholder}
+							onChange={this.handleChange}
+							onFocus={this.handleFocus}
+							onBlur={this.handleBlur}
+						/>
+						
 					</div>
+					<div className="status-icon-area">
+						{this.state.touched &&
+						<StatusIcon 
+							isValid={this.props.isValid}
+							height="18pt"
+							width="18pt"
+						></StatusIcon>}
+					</div>
+					
 				</div>
-				
+				<div className="errorMessage">
+					
+					{ (this.state.focus && this.props.context === "password" && 
+						<div className="password-tip-container">
+							<PasswordTip
+								reqs={this.props.reqs}
+								passwordProgress={this.props.passwordProgress}
+								isValid={this.props.isValid}
+								restrictedWords={this.props.restrictedWords}
+								noRestrictedWords={this.state.noRestrictedWords}
+							></PasswordTip>
+						</div>
+					)
+						||
+						<ErrorMessage
+							isVisible={this.state.showErrorMessage}
+							errorMessage={this.state.errorMessage}>
+						</ErrorMessage>
+					}
+				</div>
 			</div>
-			
-			
 		);
 	}
 }
@@ -226,6 +152,7 @@ Input.propTypes = {
 	// for passwords
 	"reqs": PropTypes.object,
 	"restrictedWords": PropTypes.array,	
+	"passwordProgress": PropTypes.obj,	
 };
 const defaultInputProps = {
 	"inputType": "text",
--- src/form.js
+++ src/form.js
@@ -3,76 +3,152 @@ import React, { Component } from "react";
 import Input from "./Input/input";
 import Submit from "./Submit/submit";
 import "./form.css";
+/* 	VERBS
+	validate: Process a new input value, check if it's okay value for the type of field it is
+	update: Take a new input and store it in the forms state
+	
+*/
 export class Form extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			"inputValidity": {},
+			"inputValidity": {
+				"email": false,
+				"username": false,
+				"password": false,
+				"passwordConfirm": false,
+			},
+			"inputValues": {
+				"email": "",
+				"username": "",
+				"password": "",
+				"passwordConfirm": "",
+			},
+			"passwordProgress": {
+				"numberOfCapitals": false,
+				"numberOfSymbols": false,
+				"numberOfNumbers": false,
+				"minimumLength": false,
+				"noRestrictedWords": false
+			},
 			"passwordReqs": {
 				"numberOfCapitals": 1,
 				"numberOfSymbols": 1,
 				"numberOfNumbers": 1,
 				"minimumLength": 8,
 				"noRestrictedWords": true,
-				"passwordsMatch": true
+				"passwordsMatch": true,
+				"restrictedWords": ["password", "user", "username"]
 			},
-			"restrictedWords": ["password", "user", "username"]
 		};
 		this.handleFormInput = this.handleFormInput.bind(this);
-		this.checkPasswordsMatch = this.checkPasswordsMatch.bind(this);
-		this.validateSubmit = this.validateSubmit.bind(this);
+		this.processSubmit = this.processSubmit.bind(this);
+
+		this.validateEmail = this.validateEmail.bind(this);
+		this.validateUsername = this.validateUsername.bind(this);
+		this.validatePassword = this.validatePassword.bind(this);
+		this.validatePasswordConfirm = this.validatePasswordConfirm.bind(this);
 	}
 
-	handleFormInput(event, isValid, context) {
+	handleFormInput(event, context) {
 		this.setState({
-			[context]: event.target.value,
-			"inputValidity": {
-				...this.state.inputValidity,
-				[context]: isValid
+			"inputValues": {
+				...this.state.inputValues,
+				[context]: event.target.value,
 			}
 		});
-		if (context === "password") {
-			this.checkPasswordsMatch(event.target.value, this.state.passwordConfirm);
-		}
-		else if (context === "passwordConfirm") {
-			this.checkPasswordsMatch(this.state.password, event.target.value);
-		}
 	}
 	
 	validateEmail(event) {
 		// regex from http://stackoverflow.com/questions/46155/validate-email-address-in-javascript
-		var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-		return re.test(event);
-	}
-
-	checkPasswordsMatch(password, confirm) {
-		let validMatch = (confirm != undefined)&&(password == confirm);
+		let re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		let isValid =  re.test(event.target.value)&&(event.target.value.length > 0);
 		this.setState({
-			"passwordReqs": {
-				...this.state.passwordReqs,
-				"passwordsMatch": validMatch
+			"inputValidity": {
+				...this.state.inputValidity,
+				"email": isValid
 			}
 		});
-		return validMatch;
 	}
 
-	validateSubmit(event) {
-		console.log("submitting");
-		const andAllReducer = (previous, currentTest) => previous&currentTest;
-		let isSubmittable = Object.values(this.state.inputValidity).reduce(andAllReducer, this.state.inputValidity.length > 0);
+	validateUsername(event) {
+		this.setState({
+			"inputValidity": {
+				...this.state.inputValidity,
+				"username": event.target.value.length > 0
+			}
+		});
+	}
+
+	validatePassword(event) {
+		let value = event.target.value;
+		let countCapitals = value.replace(/[^A-Z]/g, "").length;
+		let countNumbers = value.replace(/[^0-9]/g, "").length;
+		let countSymbols = value.replace(/[^&%$#@!?]/g, "").length;
+		let isValid = true;
+		let tests = {};
+
+		if (this.state.passwordReqs.numberOfCapitals) {
+			tests["numberOfCapitals"] = this.state.passwordReqs.numberOfCapitals <= countCapitals;
+		}
+		if (this.state.passwordReqs.numberOfNumbers) {
+			tests["numberOfNumbers"] = this.state.passwordReqs.numberOfNumbers <= countNumbers; 
+		}
+		if (this.state.passwordReqs.numberOfSymbols) {
+			tests["numberOfSymbols"] = this.state.passwordReqs.numberOfSymbols <= countSymbols; 
+		}
+		if (this.state.passwordReqs.minimumLength) {
+			tests["minimumLength"] = this.state.passwordReqs.minimumLength <= value.length; 
+		}
+		if (this.state.passwordReqs.noRestrictedWords) {
+			tests["noRestrictedWords"] = !this.state.passwordReqs.restrictedWords.includes(value); 
+		}
+
+		this.setState({
+			"passwordProgress": tests
+		});
+
+		const andAllReducer = (previous, currentTest) => previous&&currentTest;
+		isValid = Object.values(tests).reduce(andAllReducer, isValid);
+
+		let passwordConfirmNotEmpty = this.state.inputValues.passwordConfirm.length > 0;
+		let passwordsMatch = this.state.inputValues.passwordConfirm == event.target.value;
+		console.log({
+			"passwordConfirmNotEmpty": this.state.inputValues.passwordConfirm.length > 0,
+			"passwordsMatch": this.state.inputValues.passwordConfirm == event.target.value
+		});
+		this.setState({
+			"inputValidity": {
+				...this.state.inputValidity,
+				"password": isValid,
+				"passwordConfirm":  passwordConfirmNotEmpty&&passwordsMatch
+			}
+		});
+	}
+
+	validatePasswordConfirm(event) {
+		let passwordConfirmNotEmpty = event.target.value.length > 0;
+		let passwordsMatch = event.target.value == this.state.inputValues.password;
+		this.setState({
+			"inputValidity": {
+				...this.state.inputValidity,
+				"passwordConfirm": passwordConfirmNotEmpty&&passwordsMatch
+			}
+		});
+	}
+
+	processSubmit(event) {
+		const andAllReducer = (previous, currentTest) => previous&&currentTest;
+		let isSubmittable = Object.values(this.state.inputValidity).reduce(andAllReducer);
+		let o = Object.values(this.state.inputValidity);
+
 		if (isSubmittable) {
 			alert("you're good to go boss!");
 		} else {
 			alert("you fucked up kid!");
 		}
 	}
-
-	validateUsername(value) {
-		return value.length > 0;
-	}
-
-	
 
 	render() {
 		return (
@@ -83,7 +159,9 @@ export class Form extends Component {
 					invalidText="Email is invalid"
 					emptyText="Email can't be empty"
 					validator={this.validateEmail}
+					isValid={this.state.inputValidity.email}
 					onChange={this.handleFormInput}
+					ref={(email) => {this.email = email;}}
 				>
 				</Input>
 				<Input 
@@ -92,7 +170,9 @@ export class Form extends Component {
 					invalidText="Username is invalid"
 					emptyText="Username can't be empty"
 					validator={this.validateUsername}
+					isValid={this.state.inputValidity.username}
 					onChange={this.handleFormInput}
+					ref={(username) => {this.username = username;}}
 				>
 				</Input>
 				<Input 
@@ -102,8 +182,12 @@ export class Form extends Component {
 					invalidText="Password is invalid"
 					emptyText="Password can't be empty"
 					onChange={this.handleFormInput}
+					validator={this.validatePassword}
+					isValid={this.state.inputValidity.password}
 					reqs={this.state.passwordReqs}
-					restrictedWords={this.state.restrictedWords}
+					passwordProgress={this.state.passwordProgress}
+					restrictedWords={this.state.passwordReqs.restrictedWords}
+					ref={(password) => {this.password = password;}}
 				>
 				</Input>
 				<Input 
@@ -112,31 +196,15 @@ export class Form extends Component {
 					name="Confirm Password"
 					invalidText="Passwords don't match"
 					emptyText="Please confirm your password"
-					isValid={this.state.passwordReqs.passwordsMatch}
-					validator={this.checkPasswordsMatch}
+					isValid={this.state.inputValidity.passwordConfirm}
+					validator={this.validatePasswordConfirm}
 					onChange={this.handleFormInput}
+					ref={(passwordConfirm) => {this.passwordConfirm = passwordConfirm;}}
 				>
 				</Input>
 
 				<Submit value="CREATE ACCOUNT"
-					onClick={this.validateSubmit}></Submit>
-
-				<input type="file"/>
-
-				<p>email: {this.state.email}</p>
-				{
-					this.state.inputValidity.email && <p>email is valid</p>
-				}
-				<p>username: {this.state.username}</p>
-				{
-					this.state.inputValidity.username && <p>username is valid</p>
-				}
-				<p>password: {this.state.password}</p>
-				{
-					this.state.inputValidity.password && <p>password is valid</p>
-				}
-				<p>state: {this.state.state}</p>
-
+					onClick={this.processSubmit}></Submit>
 
 			</div>
 		
